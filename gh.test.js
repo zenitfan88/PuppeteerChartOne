3ae6fc02c69c601234aca7ba0ca9a1610dc384da
zenitfan88 <nikanenkov@rambler.ru>
2023-01-31 20:23:11 +0400

    fixed timeout gh

--- gh.test.js
+++ gh.test.js
@@ -1,5 +1,4 @@
 let page;
-jest.setTimeout(60 * 1000);
 
 beforeEach(async () => {
   page = await browser.newPage();
@@ -13,35 +12,36 @@ describe("Github team page tests", () => {
   
   beforeEach(async () => {
     await page.goto("https://github.com/team");
-  });
+  }, 10000);
 
   test("The h1 header content", async () => {
     const firstLink = await page.$("header div div a");
     await firstLink.click();
-    await page.waitForTimeout(6000);
+    await page.waitForSelector('h1');
     const title2 = await page.title();
     expect(title2).toEqual('GitHub for teams · Build like the best teams on the planet · GitHub');
-  });
+  }, 10000);
 
-  test("The first link attribute", async () => {
-    await page.waitForTimeout(1000);
+  test("The first link attribute", async () => {;
     const actual = await page.$eval("a", link => link.getAttribute('href') );
     expect(actual).toEqual("#start-of-content");
-  });
+  }, 3000);
 
   test("The page contains Sign in button", async () => {
     const btnSelector = ".btn-large-mktg.btn-mktg";
-    await page.waitForTimeout(5000);
+    await page.waitForSelector(btnSelector, {
+      visible: true,
+    });
     const actual = await page.$eval(btnSelector, link => link.textContent);
     expect(actual).toContain("Get started with Team")
   });
-});
+}, 1000);
 
 describe("Github customer stories page tests", () => {
 
   beforeEach(async () => {
     await page.goto(" https://github.com/customer-stories");
-  });
+  }, 10000);
 
   test("click on the link See all stories", async () => {
     const firstLink = await page.$("[class = 'link-mktg text-semibold color-fg-default py-1 f3-mktg']");
@@ -49,7 +49,7 @@ describe("Github customer stories page tests", () => {
     await page.waitForTimeout(3000);
     const actual = await page.title();
     expect(actual).toContain("GitHub's Success Stories · GitHub");
-  });
+  }, 6000);
 
     test("click on the link shopify", async () => {
     const firstLink = await page.$("[class = 'customer-story-card__background rounded overflow-hidden mb-5']");
@@ -58,7 +58,7 @@ describe("Github customer stories page tests", () => {
     const titleSelector = "[class = 'h3-mktg mb-6 mb-lg-4']";
     const actual = await page.$eval(titleSelector, link => link.textContent);
     expect(actual).toContain('Shopify keeps pushing eCommerce forward with help from GitHub tools.');
-  });
+  }, 6000);
 
     test("click on the link Start a free trial", async () => {
     const firstLink = await page.$("[class = 'sub-nav-mktg-ctas ml-lg-3']");
@@ -67,6 +67,6 @@ describe("Github customer stories page tests", () => {
     const titleSelector = "[class = 'mb-4 mb-md-8 container-xl']";
     const actual = await page.$eval(titleSelector, link => link.textContent);
     expect(actual).toContain('Pick your trial plan');
-  });
+  }, 6000);
 
 });
